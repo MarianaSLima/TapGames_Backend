acf7bae25b0faf94994cae099aa4537ae888d2e8
Mariana Dos Santos Lima <mariana.lima162@hotmail.com>
2022-08-08 17:18:21 -0300

    08/08

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,93 +1,182 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Users = require('../models/Users'); //importando o modelo de Users
 
 module.exports = {
     add: async (req, res) => {
-        const {avatar, nome, nick, email, passwordHash, score, ranking} = req.body;
-        console.log("aqui")
-        let addUser = new Users({avatar, nome, nick, email, passwordHash, score, ranking});
+        const { avatar, nome, nick, email, password, score, ranking } = req.body;
+
+        const userExist = await Users.findOne({ email });
+        if (userExist) {
+            res.json({
+                data: [],
+                error: "E-mail já cadastrado!"
+            });
+            return;
+        }
+
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        let addUser = new Users({ avatar, nome, nick, email, passwordHash, score, ranking });
         const saveUsers = await addUser.save();
-        if(!saveUsers) {
+        if (!saveUsers) {
             res.json({
                 error: 'Erro ao adicionar User'
             });
-        }else {
-            res.json({
-                data: saveUsers
-            });
-        }
-    },
-    login: async (req, res) => {
-        const { email, password} = req.body;
-        const user = await Users.findOne({email});
-
-        if(!user) {
-            res.json({
-                error: 'Erro ao logar'
-            });
+            return;
         }
 
-        if(user.passwordHash === password) {
-            res.json({
-                data: user
-            });
-        }
+        res.json({
+            data: saveUsers
+        });
+
     },
 
-    list: async(req, res) => {
+    signin: async (req, res) => {
+        const { email, password } = req.body;
+        const userExist = await Users.findOne({ email });
+
+        if (!userExist) {
+            res.json({
+                data: [],
+                error: 'O Usuário não existe!'
+            });
+            return;
+        }
+
+        const match = await bcrypt.compare(password, userExist.passwordHash);
+
+        if (!match) {
+            res.json({
+                data: [],
+                error: "Senha incorreta!"
+            });
+            return;
+        }
+        res.json({
+            data: userExist
+        });
+    },
+
+    ranking: async (req, res) => {
+        const rankingList = await Users.find({ ranking: { $gt: 0 }, score: { $ne: 0 } })
+            .sort({ ranking: 1 })
+            .limit(10)
+            .select({
+                ranking: 1,
+                avatar: 1,
+                nick: 1,
+                score: 1,
+                _id: 0
+            }).exec();
+        res.json({
+            data: rankingList
+        });
+    },
+
+
+    /*
+    list: async (req, res) => {
         const listUsers = await Users.find();
-        if(!listUsers){
+        if (!listUsers) {
             res.json({
                 error: 'Erro ao recuperar os registros'
-            });
-        }else {
-            res.json({
-                data: listUsers
-            })
-        }
-        
-    },
-    getId: async(req, res) => {
-        const id = req.params.id;
-        const listUsers = await Users.findById(id);
-        if(!listUsers){
-            res.json({
-                error: 'Erro ao recuperar os registros'
-            });
-        }else {
-            res.json({
-                data: listUsers
-            })
-        }
-    },
-    deleteId: async(req, res) => {
-        const id = req.params.id;
-        const listUsers = await Users.findByIdAndDelete(id);
-        if(!listUsers){
-            res.json({
-                error: 'Erro ao recuperar os registros'
-            });
-        }else {
-            res.json({
-                data: listUsers
-            })
-        }
-    },
-    updateId: async(req, res) => {
-        const id = req.params.id;
-        const {avatar, nome, nick, email, passwordHash, score, ranking} = req.body;
-
-        const UserUpdate = await Users.findByIdAndUpdate(id, {avatar, nome, nick, email, passwordHash, score, ranking});
-
-        if(!UserUpdate) {
-            res.json({
-                erro: 'Não foi possivel localizar o User'
             });
         } else {
             res.json({
-                data:UserUpdate
+                data: listUsers
+            })
+        }
+
+        res.json({
+            data: userExist
+        });
+
+    },
+    getId: async (req, res) => {
+        const id = req.params.id;
+        const listUsers = await Users.findById(id);
+        if (!listUsers) {
+            res.json({
+                error: 'Erro ao recuperar os registros'
+            });
+        } else {
+            res.json({
+                data: listUsers
+            })
+        }
+    },
+    deleteId: async (req, res) => {
+        const id = req.params.id;
+        const listUsers = await Users.findByIdAndDelete(id);
+        if (!listUsers) {
+            res.json({
+                error: 'Erro ao recuperar os registros'
+            });
+        } else {
+            res.json({
+                data: listUsers
+            })
+        }
+    },
+    */
+    updateId: async (req, res) => {
+        const id = req.params.id;
+        const { avatar, nome, nick, email } = req.body;
+
+        const userUpdate = await Users.findByIdAndUpdate(id, { avatar, nome, nick, email });
+
+        if (!userUpdate) {
+            res.json({
+                data: [],
+                erro: 'Não foi possivel atualizar'
+            });
+        } else {
+            res.json({
+                data: userUpdate
             });
         }
+    },
+
+    score: async (req, res) => {
+        const nick = req.params.nick;
+        const novoScore = req.params.score;
+        const user = await Users.findOne({ nick });
+
+        if (!user) {
+            res.json({
+                data: [],
+                erro: 'Não foi possivel localizar o User'
+            });
+            return;
+        }
+
+        const id = user._id;
+        const scoreAtual = user.score;
+
+        if (novoScore > scoreAtual) {
+            await Users.findByIdAndUpdate(id, { score: novoScore });
+            const gerarRanking = await Users.aggregate([
+                {
+                    $setWindowFields: {
+                        sortBy: { score: -1 },
+                        output: {
+                            ranking: {
+                                $rank: {}
+                            }
+                        }
+                    }
+                }
+            ]).exec();
+            gerarRanking.map((user => {
+                Users.updateOne({ _id: user._id }, { ranking: user.ranking }).exec();
+            }
+            ));
+        }
+        res.json({
+            data: [],
+            msg: 'Score atualizado!'
+        });
     }
 
 }
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -3,9 +3,14 @@ const UsersController = require('../controllers/UsersController');
 const router = express.Router();
 
 router.post('/signup', UsersController.add);
-router.post('/signin', UsersController.login);
+router.post('/signin', UsersController.signin);
+router.get('/ranking', UsersController.ranking);
+/*
 router.get('/list/:id', UsersController.getId);
 router.delete('/delete/:id', UsersController.deleteId);
-router.put('/list/:id', UsersController.updateId);
+*/
+router.put('/user/:id', UsersController.updateId);
+router.put('/user/:nick/score/:score', UsersController.score);
+
 
 module.exports = router;
